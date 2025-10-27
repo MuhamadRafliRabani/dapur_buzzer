7bc9da27eecc44a333d313c34ed938980bd02708
Noirrr <muhamadraflirabani@gmail.com>
2025-10-27 16:25:32 +0700

    fix categories page to client comp

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import CategoryComp from "@/components/categories-comp";
 import AppLayout from "@/components/layout/app-layout";
+import { Suspense } from "react";
 
 const CategoryPage = () => {
   return (
-    <AppLayout>
-      <CategoryComp />{" "}
-    </AppLayout>
+    <Suspense fallback={<p>Loading...</p>}>
+      <AppLayout>
+        <CategoryComp />{" "}
+      </AppLayout>
+    </Suspense>
   );
 };
 
